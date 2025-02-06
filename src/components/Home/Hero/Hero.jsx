17d3de224f73fe9ee894ace9e62874a08bd04c6f
RasulAkhundov <resul.axundov2002@mail.ru>
2025-02-07 00:11:42 +0400

    loader new changes

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -1,23 +1,35 @@
-import React from 'react';
+'use client';
+import React, { useEffect } from 'react';
 import './hero.scss';
 import Image from 'next/image';
 
 export default function Hero() {
+
+   useEffect(() => {
+      let heroText = document.querySelector('.hero__wrapper .hero-inner__wrapper .content__wrapper');
+      let heroTextHeight = heroText.getBoundingClientRect().height;
+
+      if(heroTextHeight) {
+         let loaderCenterLine = document.querySelector('.loader__wrapper .center__line');
+         loaderCenterLine.style.minHeight = `${heroTextHeight + 10}px`
+      }
+   }, [])
+
    return (
       <div className="hero__wrapper section__paddings">
-         <div className="hero-inner__wrapper">
-
-            <div className="model-render__wrapper">
-               <Image
+         <div className="model-render__wrapper">
+            {/* <Image
                   src={'/images/hero-render.png'}
                   width={335}
                   height={332}
                   quality={100}
                   alt='astral'
                   draggable={false}
-               />
-            </div>
+               /> */}
+            <iframe src='https://my.spline.design/ai-c0da7bce2d50818ca5563cef28e6db5c/' frameBorder='0' width='100%' height='100%'></iframe>
+         </div>
 
+         <div className="hero-inner__wrapper">
             <div className="content__wrapper">
                <h2 className="text__wrapper text-gradient-color">
                   Bringing ideas to life with <p>powerful animations</p> and <p>hyper-efficient workflows.</p>
